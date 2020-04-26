cc5a0e8cc8827f61b52d4db99245939427f45569
Josh Pomerenke <jtpomerenke@gmail.com>
2020-04-26 18:17:21 -0500

    hifew

--- app.py
+++ app.py
@@ -35,13 +35,13 @@ def createNotionTask(token, collectionURL, content, url):
             # get text
             text = soup.get_text()
 
-            # # break into lines and remove leading and trailing space on each
-            # lines = (line.strip() for line in text.splitlines())
-            # # break multi-headlines into a line each
-            # chunks = (phrase.strip() for line in lines for phrase in line.split("  "))
-            # # drop blank lines
-            # text = '\n'.join(chunk for chunk in chunks if chunk)
-            # md = "".join(BeautifulSoup(text, features="html.parser").findAll(text=True))
+            # break into lines and remove leading and trailing space on each
+            lines = (line.strip() for line in text.splitlines())
+            # break multi-headlines into a line each
+            chunks = (phrase.strip() for line in lines for phrase in line.split("  "))
+            # drop blank lines
+            text = '\n'.join(chunk for chunk in chunks if chunk)
+            md = "".join(BeautifulSoup(text, features="html.parser").findAll(text=True))
             mdFile = MdUtils(file_name='Example_Markdown',title='Markdown File Example')
             mdFile.create_md_file()
             print(text)
