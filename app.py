d1d0927c5fbbd39b1b1b49e446e2bd9b7a72319f
Josh Pomerenke <jtpomerenke@gmail.com>
2020-04-26 03:09:42 -0500

    test

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ import os
 from notion.client import NotionClient
 from flask import Flask
 from flask import request
-from notion.block import EmbedBlock
+from notion.block import BookmarkBlock, BasicBlock
 
 app = Flask(__name__)
 
@@ -16,9 +16,13 @@ def createNotionTask(token, collectionURL, content, url):
     row = cv.collection.add_row()
     row.title = content
     row.url = url
-    page = row.children.add_new(EmbedBlock)
-    # sets "property.source" to the URL, and "format.display_source" to the embedly-converted URL
-    page.set_source_url(url)
+    if (url):
+        page = row.children.add_new(BookmarkBlock)
+        page.link = url
+        page.title = content
+    else:
+        page = row.children.add_new(BasicBlock,title=content)
+
 
 @app.route('/create_note', methods=['GET'])
 def create_note():
