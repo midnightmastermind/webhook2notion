1154044d2c5871caa6684e0a742041f796c86546
Josh Pomerenke <jtpomerenke@gmail.com>
2020-04-27 17:06:04 -0500

    hifew

--- app.py
+++ app.py
@@ -54,7 +54,7 @@ def createNotionTask(token, collectionURL, content, url):
             doc = Document(soup.prettify(formatter="html"))
             text = doc.summary()
 
-            output = pypandoc.convert_text(output, 'gfm-raw_html', format='html')
+            output = pypandoc.convert_text(text, 'gfm-raw_html', format='html')
             rendered = convert(output)
 
 
