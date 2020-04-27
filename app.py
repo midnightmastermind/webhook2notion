113fb01ed680429bfc5fb6097a3c3abb39ac85bf
Josh Pomerenke <jtpomerenke@gmail.com>
2020-04-27 15:08:18 -0500

    hifew

--- app.py
+++ app.py
@@ -64,7 +64,7 @@ def createNotionTask(token, collectionURL, content, url):
 
                 print(relative_path)
                 with open(relative_path, 'w') as f:
-                    f.write(img.decode('utf-8'))
+                    f.write(str(img))
 
             doc = Document(soup.prettify(formatter="html"))
             text = doc.summary()
