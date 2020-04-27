d46e572b37982c37f38366f5eae5096a805a5088
Josh Pomerenke <jtpomerenke@gmail.com>
2020-04-27 14:56:05 -0500

    hifew

--- app.py
+++ app.py
@@ -57,13 +57,8 @@ def createNotionTask(token, collectionURL, content, url):
                 new_url = urllib.parse.urljoin(domain,web_url.get('src'))
                 r = http.request('GET', new_url)
                 img = r.data
-                def path2url(path):
-                    """Return file:// URL from a filename."""
-                    path = os.path.abspath(path)
-                    if isinstance(path, unicode):
-                        path = path.encode('utf8')
-                    return 'file:' + urlparse(path)
-                relative_path = path2url(str(pathlib.Path().absolute())+web_url.get('src'))
+
+                relative_path = os.path.abspath(str(pathlib.Path().absolute())+web_url.get('src'))
                 print(relative_path)
                 with open(relative_path, 'a+') as f:
                     f.write(img)
