4b68c285f79c6c628e70371d53e08fddcdb6cd0d
Josh Pomerenke <jtpomerenke@gmail.com>
2020-04-26 20:41:16 -0500

    hifew

--- app.py
+++ app.py
@@ -39,8 +39,8 @@ def createNotionTask(token, collectionURL, content, url):
 
             output = pypandoc.convert_text(text, 'md', format='html').encode('utf-8')
             newPage = row.children.add_new(PageBlock, title=doc.title())
-            print(str(output))
-            upload("", newPage)
+            test = str(output)
+            upload(test, newPage)
 
             page = row.children.add_new(BookmarkBlock)
             page.link = url
