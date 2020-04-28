92efb69f10075372a68391bc4fd3539784176d44
Josh Pomerenke <jtpomerenke@gmail.com>
2020-04-28 10:52:23 -0500

    hifew

--- app.py
+++ app.py
@@ -69,28 +69,31 @@ def createNotionTask(token, collectionURL, content, url):
                 output = output.replace("\\\\'", "\'")
                 if (output == ""):
                     break
-                else:
-                    rendered = convert(output)
-                    def convertImagePath(imagePath, mdFilePath):
-                        parsed_url = urllib.parse.urlparse(url)
-                        domain = parsed_url.scheme + '://' + parsed_url.netloc
-                        relative_url = os.path.abspath(str(pathlib.Path().absolute()) + imagePath)
-                        new_url = urllib.parse.urljoin(domain, imagePath)
-                        r = http.request('GET', new_url)
-                        img = r.data
 
-                        os.makedirs(os.path.dirname(relative_url), exist_ok=True)
-                        with open(relative_url, 'wb') as f:
-                            f.write(img)
+                rendered = convert(output)
 
-                        return Path(os.path.abspath(str(pathlib.Path().absolute()) + imagePath))
-                    # Upload all the blocks
-                    for blockDescriptor in rendered:
-                        uploadBlock(blockDescriptor, row, doc.title(),imagePathFunc=convertImagePath)
+
+                def convertImagePath(imagePath, mdFilePath):
+                    parsed_url = urllib.parse.urlparse(url)
+                    domain = parsed_url.scheme + '://' + parsed_url.netloc
+                    relative_url = os.path.abspath(str(pathlib.Path().absolute()) + imagePath)
+                    new_url = urllib.parse.urljoin(domain, imagePath)
+                    r = http.request('GET', new_url)
+                    img = r.data
+
+                    os.makedirs(os.path.dirname(relative_url), exist_ok=True)
+                    with open(relative_url, 'wb') as f:
+                        f.write(img)
+
+                    return Path(os.path.abspath(str(pathlib.Path().absolute()) + imagePath))
+                # Upload all the blocks
+                for blockDescriptor in rendered:
+                    uploadBlock(blockDescriptor, row, doc.title(),imagePathFunc=convertImagePath)
             except:
                 expanded_url = urlexpander.expand(url)
                 print(expanded_url)
                 if('imgur' in expanded_url):
+                    print("ok")
                     # client = ImgurClient(client_id, client_secret)
                     # items = client.gallery()
                     # for item in items:
