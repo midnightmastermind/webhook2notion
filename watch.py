764f523c73f7f830d7c826ca27f7888950512e4f
Josh Pomerenke <jtpomerenke@gmail.com>
2020-05-05 14:57:57 -0500

    adding watch

--- watch.py
+++ watch.py
@@ -4,6 +4,9 @@ token_v2 = "ab276d01115dd3f4ab499eb5ac4055057ddf28380755c8b77d83924cb2965f7928f1
 
 client = NotionClient(token_v2=token_v2, monitor=True, start_monitoring=True)
 
+def postBlog(record):
+    return record.title
+
 def watch_blog():
 
 	cv = client.get_collection_view(
@@ -11,9 +14,18 @@ def watch_blog():
 	)
 
 	def my_callback(record):
-		print("The record's title is now:", record.title)
+        try:
+            job = q.enqueue_call(func=postBlog, args=(record), result_ttl=5000)
+            return f'added {record.title} to Queue'
+            print(job.get_id())
+
 
 	for block_row in cv.collection.get_rows() :
 		block_row.add_callback(my_callback(block_row))
 
 watch_blog()
+
+if __name__ == '__main__':
+    app.debug = True
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
